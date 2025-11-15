27aa7a7a05b4bffadbf65397ae081222f8fecc14
amjadfaiz-ma <amuhamadafandy@ucsd.edu>
2025-11-14 19:49:18 -0800

    Update map.js

--- map.js
+++ map.js
@@ -16,3 +16,34 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+map.on('load', async () => {
+
+    // ---- Load Boston Bike Lanes ----
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+    });
+
+    map.addLayer({
+        id: 'boston-bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: bikeLaneStyle
+    });
+
+    // ---- Load Cambridge Bike Lanes ----
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson'
+    });
+
+    map.addLayer({
+        id: 'cambridge-bike-lanes',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: bikeLaneStyle
+    });
+
+    console.log("Boston + Cambridge bike lanes loaded."); 
+});
